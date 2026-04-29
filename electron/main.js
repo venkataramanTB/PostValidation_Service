c7d96267b55c2bcfa43d5bf449da43108d636088
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2026-04-29 16:28:28 +0530

    security: enable webSecurity in Electron renderer

--- electron/main.js
+++ electron/main.js
@@ -118,7 +118,7 @@ function createMainWindow() {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
-      webSecurity: false,
+      webSecurity: true,
     },
   });
   mainWindow.loadFile(frontendIndexPath());
