e45386736ce0cf326dd82a87b6b18046ef5e7ce8
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2026-04-29 11:23:05 +0530

    feat: add Electron splash screen and preload contextBridge

---
+++ electron/preload.js
@@ -0,0 +1,6 @@
+﻿const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  onBackendCrash: (callback) =>
+    ipcRenderer.on('backend-crash', (_event, message) => callback(message)),
+});
